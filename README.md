fc9910ecedf082b73ecd10c2ed26b625006dc62c
Olimjon <olimjon.ibragimov@yandex.com>
2021-02-07 12:38:26 +0100

    add working code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-TC-Resnet-TensorFlow-2
+# TC-ResNet implementation in TensorFlow 2
+
+In this project, TC-ResNet 8 and 14 architectures are implemented in TF 2.4.1.
+The goal is to provide a lightweight CNN model for Keyword Spotting with audio data in real time on mobile devices.
+
+[Original paper](https://arxiv.org/abs/1904.03814v2)
+
+[Author's implementation with Tensorflow](https://github.com/hyperconnect/TC-ResNet)
+
+[Inspired from this Keras implementation](https://github.com/tranHieuDev23/TC-ResNet)
+
+# How to use
+
+For training, testing and validation, this implementation uses [Google's Speech Command Dataset](https://ai.googleblog.com/2017/08/launching-speech-commands-dataset.html). Please download the dataset, and extract into a folder named `dataset` in the root folder of the repository.
+
+Run `main.py` to train the model.
+
+Run `live.py` to demostrate live prediction of the model in real time.
---
+++ live.py
@@ -0,0 +1,55 @@
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
+from train import get_tc_resnet_8
+from os import listdir
+from os.path import isdir, join
+# from tensorflow.keras.models import load_model
+from process_data import get_mfcc
+
+sr = 16000
+audio_length = 2
+blocksize = 100
+sd.default.samplerate = sr
+sd.default.channels = 1
+sd.default.blocksize = blocksize
+sd.default.latency = 'low'
+
+root_folder = '../dataset'
+
+classes = [item for item in listdir(root_folder) if isdir(
+    join(root_folder, item)) and not item.startswith('_')]
+num_classes = len(classes)
+
+# model = get_tc_resnet_14((321, 40), num_classes, 1.5)
+model = get_tc_resnet_8((321, 40), num_classes, 1.5)
+model.load_weights('weights.h5')
+model.summary()
+
+recent_signal = []
+recording_id = 0
+
+try:
+    while True:
+        input("Press Enter to start recording:")
+        stream = sd.InputStream()
+        stream.start()
+        print("Say the word:")
+        while True:
+            data, overflowed = stream.read(blocksize)
+            data = data.flatten()
+            recent_signal.extend(data.tolist())
+            if len(recent_signal) >= sr * audio_length:
+                recent_signal = recent_signal[:sr * audio_length]
+                break
+        stream.close()
+        sf.write(f"recording_{recording_id}.wav", np.array(recent_signal), sr)
+        print("Recording finished! Result is:")
+        mfcc = get_mfcc(np.asarray(recent_signal), sr)
+        y_pred = model.predict(np.array([mfcc]))[0]
+        result_id = np.argmax(y_pred)
+        result_prob = y_pred[result_id]
+        print("result id: " + str(result_id) + " " + classes[result_id] + " " + str(result_prob))
+        recent_signal = []
+except KeyboardInterrupt:
+    print('Record finished!')
